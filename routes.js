d3360e2ce9d54e0af3fae59e2e4e23977b7a3ab3
rodri <25470856+drigohhh@users.noreply.github.com>
2024-09-11 17:56:05 -0300

    (Primeiro Commit) Sistema inicial de Login e Registro de usuários pronto

---
+++ routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = express.Router();
+
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+
+// Rotas da home
+routes.get('/', homeController.index);
+
+// Rotas de login
+routes.get('/login/index', loginController.index);
+routes.post('/login/register', loginController.register);
+
+module.exports = routes;
